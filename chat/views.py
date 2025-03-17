f8eb6648ffe0acde1d4d91c05e22469260ab0189
K.Thirumal Reddy <klreddy2636@gmail.com>
2025-03-17 18:14:13 +0530

    Update views.py
    Added some API views

--- chat/views.py
+++ chat/views.py
@@ -13,11 +13,11 @@ from django.contrib.auth.models import User
 
 from rest_framework.views import APIView
 
-from chat.models import UserProfile
+from chat.models import Rooms, UserProfile
 from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed
 
-from chat.serializers import UserProfileSerializer
+from chat.serializers import RoomSerializer, UserProfileSerializer
 
 class VerifyTokenView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -26,9 +26,11 @@ class VerifyTokenView(APIView):
     def get(self, request):
         print("The user is ",request.user)
         user= request.user
+        allUsers = UserProfile.objects.all()
+        users = UserProfileSerializer(allUsers,many=True)
         user_profile = UserProfile.objects.get(user=user)
         user_profile=UserProfileSerializer(user_profile)
-        return Response({'user_profile': user_profile.data}, status=status.HTTP_200_OK)
+        return Response({'user_profile': user_profile.data , 'allUsers':users.data}, status=status.HTTP_200_OK)
         # try:
         #     return Response({'message': 'Token is valid'}, status=status.HTTP_200_OK)
         # except AuthenticationFailed:
@@ -40,6 +42,7 @@ class HomeView(View):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication, SessionAuthentication, BasicAuthentication]
     def get(self, request):
+        
         return render(request, 'home.html')
     
 
@@ -106,10 +109,24 @@ class PeopleAPIView(APIView):
     def get(self, request):
         user= request.user
         users = UserProfile.objects.exclude(user=user)
-        print("The users are ",users)
+        user_profile = UserProfile.objects.get(user=user)
+        
+        friends = user_profile.friends
+        friends = UserProfileSerializer(friends,many=True)
+        print("The users friends are: ",friends.data)
+        friendsrequested = user_profile.friendsrequested
+
+        friendrequests = user_profile.friendrequests
+
+        friendrequests = UserProfileSerializer(friendrequests,many=True)
+        print("The friend requests are",friendrequests.data)
+
+        
+        friendsrequested = UserProfileSerializer(friendsrequested, many=True)
+        print("The users requested are ",friendsrequested.data)
         users = UserProfileSerializer(users, many=True)
-        print("The users are ",users.data)
-        return Response({"user_profiles": users.data})
+        
+        return Response({"user_profiles": users.data, "friends": friends.data, "friendsrequested": friendsrequested.data,"friendrequests": friendrequests.data})
 
 
 class ProfileView(View):
@@ -168,21 +185,26 @@ class AboutView(View):
     
 class FriendRequestView(View):
     def get(self, request):
+        
         return render(request, 'requests.html')
+
 
 class FriendRequestAPIView(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication, SessionAuthentication, BasicAuthentication]
     def get(self, request):
         user= request.user
+        print("The user is %s" % user)
         friend_requests = UserProfile.objects.get(user=user).friendrequests
+        print("Friend requests: ",friend_requests)
+        
         friend_requests = UserProfileSerializer(friend_requests, many=True)
-        return Response({"friend_requests": friend_requests.data})
+        return Response({"requests": friend_requests.data})
     
     def post(self, request):
         user= request.user
         user_id = request.data.get('addFriend')
-
+        user_profile = UserProfile.objects.get(user=user)
 
         friend = request.data.get('addFriend')
         print("The id is: ",user_id)
@@ -191,10 +213,150 @@ class FriendRequestAPIView(APIView):
         print("The friend is: ",friend)
         if friend:
             friend = UserProfile.objects.get(user=friend)
-            friend.friendrequests.add(user)
-            friend.save()
+            print("The firend is ",friend)
+            if friend not in user.friends.all():
+                user_profile.friendsrequested.add(friend.user)
+                friend.friendrequests.add(user)
+                friend.save()
+            else:
+                return Response({"error": "Hes Already a Friedn!"}, status=400)
 
         
             return Response({"success": "Friend request sent successfully!"})
         else:
             return Response({"error": "User not found!"}, status=404)
+        
+
+
+class FriendsAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication, SessionAuthentication, BasicAuthentication]
+
+    def get(self, request):
+        user= request.user
+        print("The user is %s" % user)
+        friends = UserProfile.objects.get(user=user).friends
+        
+        friends = UserProfileSerializer(friends, many=True)
+        return Response({"friends": friends.data})
+    def post(self,request):
+        user= request.user
+        user_id = request.data.get('addFriend')
+        friend = User.objects.get(user=user_id)
+        print("The id is: ",user_id)
+        user = UserProfile.objects.get(user=user)
+        UserProfile.friends.add(friend)
+        UserProfile.friendrequests.remove(friend)
+        print("The firends are",user.friends)
+        
+    def put(self,request):
+        ##** Here the request user is accepting friend request
+        ##** The friend has requested it so adding him to friends and removing the request
+        user= request.user
+        user_profile = UserProfile.objects.get(user=user)
+        friend_id = request.data.get('addFriend')
+        friend = User.objects.get(id=friend_id)
+        print("The id is: ",friend_id)
+        if friend:
+            print("Inside Friend",friend)
+            friend = UserProfile.objects.get(user=friend)
+            print("The firend is ",friend.username)
+            user_profile.friends.add(friend.user)
+            friend.friends.add(user)
+            friend.friendsrequested.remove(user_profile.user)
+            user_profile.friendrequests.remove(friend.user)
+            
+            friend.save()
+            return Response({"success": "Friend added successfully!"})
+        else:
+            return Response({"error": "User not found!"}, status=404)
+        
+
+
+def Room(request,room_name):
+    print("room_name:----------- ",room_name)
+    return render(request, 'Room.html', {'room_name': room_name})
+
+
+
+class CreateRoomView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+ 
+    def get(self,request):
+        user_id = request.user.id
+        print("user_id:----------- ",user_id)
+        user_id = UserProfile.objects.get(user__id = user_id)
+        print("user_id:----------- 2",user_id.id)
+        rooms = Rooms.objects.filter(users__id=user_id.id)
+        print("rooms------------roomms",rooms)
+        rooms = RoomSerializer(rooms , many=True)
+        print(rooms)
+ 
+        return Response({'rooms': rooms.data})
+    def post(self, request, *args, **kwargs):
+        room_name = request.data.get("room_name")
+        user_ids = request.data.get("users", [])
+        print("user_ids:----------- ", user_ids)
+        if not room_name or not user_ids:
+            return Response(
+                {"error": "Room name and users are required."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+ 
+        # Get user instances for the provided IDs
+        users = []
+        print("----------------------",request.user)
+        try:
+            creator = UserProfile.objects.get(user=request.user)
+            print('creator',creator)
+            users.append(creator)
+        except UserProfile.DoesNotExist:
+            print(f"User with id {request.user.id} not found.")
+ 
+        for id in user_ids:
+            user = UserProfile.objects.get(id=id)
+            users.append(user)
+        print('users',users)
+       
+ 
+        room = Rooms.objects.create(room_name=room_name)
+        print('rooms',room)
+        room.users.add(*users)  # Add users to the room's many-to-many field
+       
+        return Response({
+            'success': True,
+            'message': 'Room created successfully!',
+            'room': RoomSerializer(room).data
+        }, status=status.HTTP_201_CREATED)
+ 
+
+
+
+
+
+
+class RoomDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request,*args,**kwargs):
+
+        room=request.query_params.get("room_name")
+        print("The rorom name is",room)
+
+        if not room:
+            return Response({"error": "room_name is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            room = Rooms.objects.get(room_name=room)
+            print("Got the rrom object",room.users)
+        except Rooms.DoesNotExist:
+            return Response({"error": "Room Not OFund"}, status=status.HTTP_404_NOT_FOUND)
+        users=room.users.all()
+        users = UserProfileSerializer(users,many=True)
+        print("THe users are ",users)
+        
+        return Response({
+            'users': users.data
+        })
