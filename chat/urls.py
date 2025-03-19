bd12cd08d0812dbc03599c59af7c08283eb1a7cd
K.Thirumal Reddy <klreddy2636@gmail.com>
2025-03-19 17:54:01 +0530

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -6,10 +6,17 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', Register.as_view(), name='register'),
     path('people/', PeopleView.as_view(), name='people'),
+    path('requests/', FriendRequestView.as_view(), name='request'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('about/', AboutView.as_view(), name='about'),
+    path("room/<str:room_name>",Room , name='room'),
+    path('chat/',PersonalChatView.as_view(), name='chat'),
+    
+    path('API/room/', RoomDetailView.as_view(),name='roomdetail'),
     path('API/profile/',ProfileUpdate.as_view(), name='profileupdate'),
     path('API/verify_token/', VerifyTokenView.as_view(), name='verify_token'),
     path('API/people/', PeopleAPIView.as_view(), name='people'),
     path('API/frndrqst/', FriendRequestAPIView.as_view(), name='friendrequest'),
+    path('API/friends/', FriendsAPIView.as_view(), name='friend'),
+    path('api/create-room/', CreateRoomView.as_view(), name='createroom')
     ]
